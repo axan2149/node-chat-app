30f9e21081cae97cad46e576652374a7bdd1dc28
Martin Rozycki <mrozycki@imshome.com>
2017-04-20 15:21:46 -0400

    added connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -2,18 +2,31 @@
  * Created by mrozycki on 4/20/2017.
  */
 const path =require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
+const port = process.env.PORT || 3000;
 
-const express = require('express');
-
-const app = express();
-
-var PORT = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () =>{
+
+io.on('connection', (socket) => {
     "use strict";
-    console.log(`starting server on port ${PORT}`);
+    console.log('New User Connected');
+    
+    socket.on('disconnect', () =>{
+        console.log('Client Disconnected');
+    });
+});
+
+server.listen(port, () =>{
+    "use strict";
+    console.log(`starting server on port ${port}`);
+    console.log(`http://localhost:${port}`);
 });
